e58f6b3c9a3a2a8ef57aa57b7924b53610e82c34
Luis Andrade Macias <luisenriqueandrademacias@gmail.com>
2022-04-06 23:30:08 -0500

    Admin layout was divided inc omponents

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -1,18 +1,27 @@
 <template>
-  <body class="g-sidenav-show bg-gray-100">
-    <div class="min-height-300 bg-primary position-absolute w-100"></div>
-    <slide-bar></slide-bar>
-    <main class="main-content position-relative border-radius-lg ps">
-      <!-- Navbar -->
-      <header></header>
-      <!-- End Navbar -->
-    </main>
+<body class="g-sidenav-show   bg-gray-100">
+  <div class="min-height-300 bg-primary position-absolute w-100"></div>
+  <slide-bar></slide-bar>
+  <main class="main-content position-relative border-radius-lg ">
+    <!-- Navbar -->
+   <navbar></navbar>
+    <!-- End Navbar -->
+    <div class="container-fluid py-4">
+      <div class="row">
+        <div class="col-xl-12 col-sm-6 mb-xl-0 mb-4">
+          <div class="card">
+           <slot></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
   </body>
 </template>
 
 <script>
-import { SlideBar } from '@/Pages/Template/SlideBar'
-import { Header } from '@/Pages/Template/Header'
+import  SlideBar  from '@/Pages/Template/SlideBar'
+import  Navbar  from '@/Pages/Template/Header'
 
 export default ({
   data(){
@@ -20,7 +29,7 @@ export default ({
     },
     components: {
       SlideBar,
-      Header
+      Navbar
   }
 })
 </script>
--- resources/js/Pages/Dashboard/DashboardIndex.vue
+++ resources/js/Pages/Dashboard/DashboardIndex.vue
@@ -1,11 +1,33 @@
 <template>
     <div>
-
+        <admin-layout>
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="col-8">
+                  <div class="numbers">
+                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Today's Money</p>
+                    <h5 class="font-weight-bolder">
+                      $53,000
+                    </h5>
+                    <p class="mb-0">
+                      <span class="text-success text-sm font-weight-bolder">+55%</span>
+                      since yesterday
+                    </p>
+                  </div>
+                </div>
+                <div class="col-4 text-end">
+                  <div class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
+                    <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
+            </div>        
+    </admin-layout>
     </div>
 </template>
 
 <script>
-import { AdminLayout } from '@/Layouts/AdminLayout.vue'
+import  AdminLayout  from '@/Layouts/AdminLayout.vue'
 
 export default ({
   data(){
