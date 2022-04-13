d980b5ebe11e52b6c424be6fbb36bf9d77816f15
Luis Andrade Macias <luisenriqueandrademacias@gmail.com>
2022-04-13 01:05:00 -0500

    feat: :sparkles: Wizard for register user and company was created

--- database/migrations/2022_04_13_054539_create_companies_table.php
+++ database/migrations/2022_04_13_054539_create_companies_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Company\Company;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name',255);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->foreignIdFor(Company::class)->nullable();
-            $table->rememberToken();
+            $table->string('phone',255);
+            $table->string('fiscal_address',255)->nullable();
+            $table->string('RFC',255)->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -33,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('companies');
     }
 };
